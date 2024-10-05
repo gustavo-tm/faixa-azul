fee7a3fc677df140e49e82bbedfb1084d80dbd58
gustavo-tm <gtheilm@gmail.com>
2024-10-05 11:21:34 -0300

    dados OSM tratados

---
+++ download_infosiga.R
@@ -0,0 +1,77 @@
+library(tidyverse)
+
+# Download ----
+
+download.dados <- function(){
+  dir.create("dados_brutos/")
+  download.file("http://painelderesultados.infosiga.sp.gov.br/bases/acidentes_naofatais.csv",
+                "dados_brutos/acidentes_naofatais.csv")
+  download.file("http://painelderesultados.infosiga.sp.gov.br/bases/obitos_publico.csv",
+                "dados_brutos/acidentes_fatais.csv")
+}
+
+if (!file.exists("dados_brutos")){download.dados()}
+
+# Tratamento ----
+
+arrumar.dados <- function(){
+  dir.create("dados_tratados/")
+  
+  df <- data.table::fread("dados_brutos/acidentes_naofatais.csv", encoding = "Latin-1") |>
+    rename(dia = "Dia do Acidente",
+           nm_municipio = "Município",
+           ano_mes = "Ano/Mês do Acidente",
+           horario = "Hora do Acidente",
+           data = "Data do Acidente") |> 
+    filter(nm_municipio == "SAO PAULO") |>  
+    mutate(data = make_datetime(year = year(data), 
+                                month = month(data), 
+                                day = as.numeric(dia), 
+                                hour = as.numeric(str_sub(horario, 1, 2)),
+                                min = as.numeric(str_sub(horario, 4, 5))))
+  
+  df |> 
+    select(ID, 
+           data,
+           logradouro = "Logradouro",
+           numero = "Numero/KM",
+           latitude = "LAT_(GEO)",
+           longitude = "LONG_(GEO)",
+           feridos_graves = "Pessoas Envolvidas - Grave",
+           feridos_leves = "Pessoas Envolvidas - Leve") |> 
+    mutate(numero = ifelse(numero == "NAO DISPONIVEL", NA, numero)) |> 
+    write_csv("dados_tratados/acidentes.csv")
+  
+  df |> 
+    rename(motocicletas = "Veículos Envolvidos - Motocicleta",
+           feridos_grave = "Pessoas Envolvidas - Grave",
+           feridos_leve = "Pessoas Envolvidas - Leve") |> 
+    group_by(ano = year(data), mes = month(data), logradouro = Logradouro) |> 
+    summarize(acidentes = n(),
+              acidentes_feridos = sum(ifelse(feridos_grave > 0 | feridos_leve > 0, 1, 0)),
+              acidentes_moto = sum(ifelse(motocicletas > 0, 1, 0)),
+              acidentes_moto_feridos = sum(ifelse((feridos_grave > 0 | feridos_leve > 0) & motocicletas > 0, 1, 0))) |> 
+    right_join(expand_grid(data = seq.Date(from = df$data |> min() |> as.Date(), 
+                                           to = df$data |> max() |> as.Date(), 
+                                           by = "month"),
+                           logradouro = df |> distinct(Logradouro) |> pull(Logradouro)) |> 
+                 mutate(mes = month(data),
+                        ano = year(data)) |> 
+                 select(-data)) |> 
+    mutate(across(everything(), ~ replace_na(.x, 0))) |> 
+    arrange(logradouro, desc(ano), desc(mes)) |> 
+    write_csv("dados_tratados/logradouros.csv")
+}
+
+if (!file.exists("dados_tratados")){arrumar.dados()}
+
+
+
+
+
+
+
+
+
+
+
---
+++ download_osm.R
@@ -0,0 +1,58 @@
+library(tidyverse)
+library(osmdata)
+library(sf)
+library(mapview)
+
+# https://cran.r-project.org/web/packages/osmdata/vignettes/osmdata.html
+# https://rspatialdata.github.io/osm.html
+
+# https://wiki.openstreetmap.org/wiki/Map_features
+available_features()
+
+osm <- getbb('São Paulo') |> 
+  opq(bbox = _) |> 
+  add_osm_feature(key = 'highway', value = c(
+    "motorway", "trunk", "primary", "secondary", "tertiary", "unclassified", "residential", "service",
+    "motorway_link", "trunk_link", "primary_link", "secondary_link", "motorway_junction",
+    "speed_camera"))  |> 
+  osmdata_sf()
+
+gg <- osm$osm_lines |> 
+  ggplot() +
+  geom_sf(lwd = .1) +
+  theme_void()
+
+ggsave("output/vias_osm.pdf", gg, width = 30, height = 40)
+
+logradouros.OSM <- as_tibble(osm$osm_lines) |> 
+  select(id_osm = osm_id,
+         logradouro = name,
+         logradouro_alt1 = alt_name,
+         logradouro_alt2 = alt_name1,
+         logradouro_alt3 = alt_name_1,
+         logradouro_ref = ref,
+         tipo_via = highway,
+         faixas = lanes,
+         limite_velocidade = maxspeed,
+         limite_velocidade_pesados = "maxspeed:hgv",
+         motocicleta = motorcycle,
+         mao_unica = oneway,
+         superficie = surface,
+         geometry) 
+
+logradouros.OSM |> write_csv("dados_tratados/logradouros_osm.csv")
+
+logradouros.OSM |>  
+  st_as_sf() |>  
+  mapview() |> 
+  mapshot(url = "output/logradouros_osm.html")
+
+distritos <- read_sf("dados_tratados/distrito/SIRGAS_SHP_distrito.shp") |> 
+  st_set_crs("epsg:31983") |> 
+  filter(ds_nome == "ITAIM BIBI")
+
+logradouros.OSM |> 
+  st_as_sf() |> st_transform("epsg:31983") |> 
+  st_crop(distritos) |>
+  mapview() |> 
+  mapshot(url = "output/logradouros_osm_itaim.html")
