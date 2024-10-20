a146e45890b58a185a3a964106320bef5c12e4b2
gustavo-tm <gtheilm@gmail.com>
2024-10-20 20:28:07 -0300

    explicativo st_distance() poligonos

--- auxiliares/explicacao_nearest.R
+++ auxiliares/explicacao_nearest.R
@@ -8,3 +8,24 @@ plot(c(linha, ponto, st_cast(linha, "MULTIPOINT")))
 st_distance(linha, ponto)
 st_distance(st_cast(linha, "MULTIPOINT"), ponto)
 sqrt(2)
+
+quadrado <- st_polygon(list(
+  matrix(c(0,1,
+           0,-1,
+           -2,-1,
+           -2,1,
+           0,1), 
+         ncol = 2, byrow = T)))
+
+triangulo <- st_polygon(list(
+  matrix(c(1,0,
+           2,-1,
+           2,1,
+           1,0), 
+         ncol = 2, byrow = T)))
+
+plot(c(quadrado, st_cast(quadrado, "MULTIPOINT"),
+       triangulo, st_cast(triangulo, "MULTIPOINT")))
+
+st_distance(quadrado, triangulo)
+st_distance(st_cast(quadrado, "MULTIPOINT"), st_cast(triangulo, "MULTIPOINT"))
