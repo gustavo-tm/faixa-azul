70352da494aa005557e603858c3b34aeaa28ee0e
Gustavo Theil <gtheilm@gmail.com>
2024-12-03 20:20:43 -0300

    mapa faixa azul

--- tidy_faixa_azul.R
+++ tidy_faixa_azul.R
@@ -5,7 +5,7 @@ faixa_azul.selecao <- read_csv("dados_tratados/faixa_azul_selecao.csv", col_type
 trechos <- st_read("banco_dados/trechos.gpkg")
 
 
-faixa_azul.selecao |> 
+faixa_azul <- faixa_azul.selecao |> 
   left_join(trechos) |> 
   mutate(logradouro = logradouro |> 
            stringi::stri_trans_general("latin-ascii") |> 
@@ -18,6 +18,37 @@ faixa_azul.selecao |>
               distinct() |>
               group_by(logradouro) |> 
               filter(n() == 1)) |> 
-  select(-logradouro) |> 
+  select(-logradouro) 
+
+faixa_azul |> 
   write_csv("banco_dados/faixa_azul.csv")
 
+
+distrito <- st_read("dados_tratados/distrito/SIRGAS_SHP_distrito.shp") |> 
+  st_set_crs("epsg:31983") |> 
+  summarize(geometry = st_union(geometry) |> st_simplify(dTolerance = 100))
+
+trechos.mapa <- trechos |> 
+  filter(!tipo_via %in% c("service", "unclassified")) |> 
+  st_transform("epsg:31983") |> 
+  st_intersection(distrito)
+  
+  
+gg <- ggplot() +
+  geom_sf(data = distrito,
+          aes(geometry = geometry), colour = "grey25", fill = "grey98") +
+  geom_sf(data = trechos.mapa |> 
+            filter(!tipo_via %in% c("trunk", "primary", "secondary")),
+          aes(geometry = st_simplify(geom, dTolerance = 10)), colour = "grey50", lwd = .15) +
+  geom_sf(data = trechos.mapa |> 
+            filter(tipo_via %in% c("trunk", "primary", "secondary")),
+          aes(geometry = st_simplify(geom, dTolerance = 10)), colour = "#3c3744", lwd = .3) +
+  geom_sf(data = trechos.mapa |> 
+            semi_join(faixa_azul),
+          aes(geometry = geom), colour = "white", lwd = 1.4, alpha = .9) +
+  geom_sf(data = trechos.mapa |> 
+            semi_join(faixa_azul),
+          aes(geometry = geom), colour = "#090c9b", lwd = 1) +
+  theme_void()
+
+ggsave("output/mapa-faixa-azul.pdf", gg, width = 30, height = 40)
