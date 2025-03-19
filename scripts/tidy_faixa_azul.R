6b7037522086ae3c656fe1317e8d7f0f587f4abe
Gustavo Theil <gtheilm@gmail.com>
2025-03-19 09:43:36 -0300

    atualizacao faixa azul

--- scripts/tidy_faixa_azul.R
+++ scripts/tidy_faixa_azul.R
@@ -3,27 +3,29 @@ library(sf)
 
 
 tidy_faixa_azul <- function(trechos){
-  faixa_azul.vias <- readxl::read_excel("dados_tratados/faixa_azul_vias.xlsx")
-  faixa_azul.selecao <- read_csv("dados_tratados/faixa_azul_selecao.csv", col_types = list(id_osm = "c"))
-
+  # faixa_azul.vias <- readxl::read_excel("dados_tratados/faixa_azul_vias.xlsx")
+  # faixa_azul.selecao <- read_csv("dados_tratados/faixa_azul_selecao.csv", col_types = list(id_osm = "c"))
+  # 
+  # 
+  # faixa_azul <- faixa_azul.selecao |> 
+  #   left_join(trechos) |> 
+  #   mutate(logradouro = logradouro |> 
+  #            stringi::stri_trans_general("latin-ascii") |> 
+  #            str_to_upper() |> 
+  #            str_replace_all("[[:punct:]]", "")) |> 
+  #   select(id_osm, logradouro) |> 
+  #   left_join(faixa_azul.vias |> 
+  #               mutate(data_implementacao = make_date(year = ano, month = mes)) |> 
+  #               select(logradouro = logradouro_osm, data_implementacao) |> 
+  #               distinct() |>
+  #               group_by(logradouro) |> 
+  #               filter(n() == 1)) |> 
+  #   mutate(data_implementacao = case_when(logradouro == "AVENIDA SANTOS DUMONT" & id_osm == "4331480" ~ make_date(year = 2023, month = 10),
+  #                                         logradouro == "AVENIDA SANTOS DUMONT" & id_osm != "4331480" ~ make_date(year = 2024, month = 4),
+  #                                         .default = data_implementacao)) |> 
+  #   select(-logradouro) 
   
-  faixa_azul <- faixa_azul.selecao |> 
-    left_join(trechos) |> 
-    mutate(logradouro = logradouro |> 
-             stringi::stri_trans_general("latin-ascii") |> 
-             str_to_upper() |> 
-             str_replace_all("[[:punct:]]", "")) |> 
-    select(id_osm, logradouro) |> 
-    left_join(faixa_azul.vias |> 
-                mutate(data_implementacao = make_date(year = ano, month = mes)) |> 
-                select(logradouro = logradouro_osm, data_implementacao) |> 
-                distinct() |>
-                group_by(logradouro) |> 
-                filter(n() == 1)) |> 
-    mutate(data_implementacao = case_when(logradouro == "AVENIDA SANTOS DUMONT" & id_osm == "4331480" ~ make_date(year = 2023, month = 10),
-                                          logradouro == "AVENIDA SANTOS DUMONT" & id_osm != "4331480" ~ make_date(year = 2024, month = 4),
-                                          .default = data_implementacao)) |> 
-    select(-logradouro) 
+  faixa_azul <- read_csv("dados_tratados/faixa_azul.csv", col_types = list(id_osm = "c"))
   
   return(faixa_azul)
   # faixa_azul |> 
