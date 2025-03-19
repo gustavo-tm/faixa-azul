54d90a2ac65589bd26b9ae73e302b23e5a5c79fc
Gustavo Theil <gtheilm@gmail.com>
2025-03-19 10:09:57 -0300

    clean _targets file

--- _targets.R
+++ _targets.R
@@ -1,49 +1,19 @@
-# Created by use_targets().
-# Follow the comments below to fill in this target script.
-# Then follow the manual to check and run the pipeline:
-#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline
-
-# Load packages required to define the pipeline:
 library(targets)
 library(visNetwork)
 # library(tarchetypes) # Load other packages as needed.
 
-# Set target options:
+workers <- 8
+
+# para garantir que osmdata vai funcionar
+assign("has_internet_via_proxy", TRUE, environment(curl::has_internet))
+
+
 tar_option_set(
   packages = c("tidyverse", "sf", "osmdata", "fuzzyjoin", "stringdist", "did", "gt", "circlize", "igraph"), 
   error = "trim",
   # format = "qs", # Optionally set the default storage format. qs is fast.
-  #
-  # Pipelines that take a long time to run may benefit from
-  # optional distributed computing. To use this capability
-  # in tar_make(), supply a {crew} controller
-  # as discussed at https://books.ropensci.org/targets/crew.html.
-  # Choose a controller that suits your needs. For example, the following
-  # sets a controller that scales up to a maximum of two workers
-  # which run as local R processes. Each worker launches when there is work
-  # to do and exits if 60 seconds pass with no tasks to run.
-  #
-    # controller = crew::crew_controller_local(workers = 6)
-  #
-  # Alternatively, if you want workers to run on a high-performance computing
-  # cluster, select a controller from the {crew.cluster} package.
-  # For the cloud, see plugin packages like {crew.aws.batch}.
-  # The following example is a controller for Sun Grid Engine (SGE).
-  # 
-  #   controller = crew.cluster::crew_controller_sge(
-  #     # Number of workers that the pipeline can scale up to:
-  #     workers = 10,
-  #     # It is recommended to set an idle time so workers can shut themselves
-  #     # down if they are not running tasks.
-  #     seconds_idle = 120,
-  #     # Many clusters install R as an environment module, and you can load it
-  #     # with the script_lines argument. To select a specific verison of R,
-  #     # you may need to include a version string, e.g. "module load R/4.3.2".
-  #     # Check with your system administrator if you are unsure.
-  #     script_lines = "module load R"
-  #   )
-  #
-  # Set other options as needed.
+
+  controller = crew::crew_controller_local(workers = workers)
 )
 
 # Run the R scripts in the R/ folder with your custom functions:
@@ -54,11 +24,8 @@ tar_source("scripts/tidy_faixa_azul.R")
 tar_source("scripts/match.R")
 tar_source("scripts/did.R")
 tar_source("scripts/did_het.R")
-# tar_source("other_functions.R") # Source other scripts as needed.
 
-assign("has_internet_via_proxy", TRUE, environment(curl::has_internet))
 
-# Replace the target list below with your own:
 list(
   # 1. SINISTROS ----
   tar_target(
